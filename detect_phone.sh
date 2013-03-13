b8d89b5466e67289a47e6ce9c1c8913cd53d00dc
Xavier Naveira <xavier.naveira@ongame.com>
2013-03-13 10:30:13 +0100

    Modified to work with XFCE

--- detect_phone.sh
+++ detect_phone.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # author: Vasilis.Vlachoudis@cern.ch
-# version: 0.0
-# date: 6 Feb 2012
+# date: 13 Mar 2013
+# modified by: xnaveira ##*at*$$ gmail.com
+# This is a modified version for XFCE
 
 BTHW="xx:xx:xx:xx:xx:xx"  # Enter your Phone Bluetooth hardware address
-LOCKPRG="kscreenlocker"		# Screen saver program
+LOCKPRG="xscreensaver"		# Screen saver program
 SLEEP=5
 
 ME=`whoami`
 while true
-do	# Run only if screen is locked
-	PID=`pgrep -u $ME $LOCKPRG | head -1`
-	if [ .$PID != . ]; then
-		# Ping the phone (needs sudo permissions
-		# chmod u+s /usr/bin/l2ping
-		l2ping -t 5 -c 1 $BTHW >/dev/null 2>/dev/null
-		RC=$?
-		if [ $RC = 0 ]; then
-			# Phone present...
-			kill $PID
-			xset dpms force on
-		fi
+do	
+        sleep 15 #give me time to run away
+	# Ping the phone (needs sudo permissions
+	# chmod u+s /usr/bin/l2ping
+	l2ping -t 5 -c 1 $BTHW >/dev/null 2>/dev/null
+	RC=$?
+	if [ $RC = 0 ]; then
+		# Phone present...
+		pkill $LOCKPRG
+                $LOCKPRG& #you want it running in the background to be able to lock the screen afterwards
+		xset dpms force on
 	fi
 	sleep $SLEEP
 done
